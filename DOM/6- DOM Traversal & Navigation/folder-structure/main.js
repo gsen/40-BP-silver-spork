f980a0c285e8d3207f28d11a7ee688dda10639bd
Gaurav Sen <gaurav.s3n@gmail.com>
2026-01-04 17:31:29 +0530

    Added home work question

---
+++ DOM/6- DOM Traversal & Navigation/folder-structure/main.js
@@ -0,0 +1,22 @@
+let files = {
+    name: "root",
+    children: [{
+        name: "folder 1",
+        children: [{
+            name: "file 1.1"
+        }, {
+            name: "file 1.2"
+        }]
+    }, {
+        name: "folder 2",
+        children: [
+            { name: "file 2.1" },
+            {
+                name: "sub folder",
+                children: [{
+                    name: "sub file 1"
+                }]
+            }
+        ]
+    }]
+}
--- DOM/6- DOM Traversal & Navigation/otp-verfication/main.js
+++ DOM/6- DOM Traversal & Navigation/otp-verfication/main.js
@@ -26,24 +26,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
 })
 
 
-let files = {
-    name: "root",
-    children: [{
-        name: "folder 1",
-        children: [{
-            name: "file 1.1"
-        }, {
-            name: "file 1.2"
-        }]
-    }, {
-        name: "folder 2",
-        children: [
-            { name: "file 2.1" },
-            {
-                name: "sub folder", children: [{
-                    name: "sub file 1"
-                }]
-            }
-        ]
-    }]
-}
