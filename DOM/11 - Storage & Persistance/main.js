2ecae4e50245388dabd7e5e766f0ce883ac4c193
Gaurav Sen <gaurav.s3n@gmail.com>
2026-01-16 21:11:50 +0530

    11, 12 - Session storage, Inheritance & OOPS

--- DOM/11 - Storage & Persistance/main.js
+++ DOM/11 - Storage & Persistance/main.js
@@ -34,10 +34,47 @@ localStorage.setItem("username", "john")
 localStorage.setItem("password", "122434")
 
 // clears all items from local storage
-localStorage.clear();
+// localStorage.clear();
+const FORM_DATA = "user-info";
+function loadFormDataFromStorage() {
+    let formData = localStorage.getItem(FORM_DATA);
 
+    if (formData) {
+        formData = JSON.parse(formData);
+
+    }
+    return formData;
+}
+
+function preFillFormData(savedData, form) {
+
+    if (savedData) {
+
+
+        let { firstName, lastName, gender } = savedData;
+        let firstNameInput = document.querySelector("#firstName");
+        firstNameInput.value = firstName;
+        let lastNameInput = document.querySelector("#lastName");
+        lastNameInput.value = lastName;
+
+        let genderInput = document.querySelector("#gender");
+        genderInput.value = gender;
+
+
+
+    }
+
+    // document.querySelector("#firstName").value = firstName;
+
+
+}
 document.addEventListener("DOMContentLoaded", function () {
     let form = document.querySelector("#user-info");
+
+    let savedData = loadFormDataFromStorage();
+    preFillFormData(savedData, form);
+
+
 
     form.addEventListener("input", function () {
         let formData = new FormData(this);
@@ -45,12 +82,28 @@ document.addEventListener("DOMContentLoaded", function () {
         let lastName = formData.get("lastName")
         let gender = formData.get("gender");
         console.log();
-        localStorage.setItem("user-info", JSON.stringify({ firstName, lastName, gender }))
+        localStorage.setItem(FORM_DATA, JSON.stringify({ firstName, lastName, gender }))
 
     })
 
     form.addEventListener("submit", function () {
 
+    })
+    // storage event is triggered when storage(local/session) is updated in another tab
+    // here we are syncing the data for all the other open tabs
+    window.addEventListener("storage", (event) => {
+        if (event.key === FORM_DATA) {
+            let { firstName, lastName, gender } = JSON.parse(event.newValue);
+            let firstNameInput = document.querySelector("#firstName");
+            firstNameInput.value = firstName;
+            let lastNameInput = document.querySelector("#lastName");
+            lastNameInput.value = lastName;
+
+            let genderInput = document.querySelector("#gender");
+            genderInput.value = gender;
+
+
+        }
     })
 })
 
