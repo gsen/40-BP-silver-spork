b432d76f0c8e98e439ea040ca66f24b29c407932
Gaurav Sen <gaurav.s3n@gmail.com>
2026-01-18 15:20:19 +0530

    13 - Drag and drop events, file handling with drag and drop

--- DOM/13 - Advanced Events & Interactions/drag events/main.js
+++ DOM/13 - Advanced Events & Interactions/drag events/main.js
@@ -2,24 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let insideDropTarget = false;
     const target = document.querySelector("#target");
-    const draggableElement = document.querySelector("[draggable='true']");
+    const draggableElements = document.querySelectorAll("[draggable='true']");
 
-    draggableElement.addEventListener("dragstart", e => {
-        console.log("element has started dragging");
-
-    })
+    draggableElements.forEach(element => {
+        element.addEventListener("dragstart", e => {
+            console.log("element has started dragging");
+            e.dataTransfer.setData("text/plain", e.target.id);
+        })
+    });
     target.addEventListener("dragenter", (e) => {
         console.log("draggable element has entered")
         console.log(e);
 
-        insideDropTarget = true
-        // if (insideDropTarget) {
-        //     let newPara = document.createElement("p");
-        //     newPara.textContent = e.target.textContent;
-        //     target.append(newPara);
-        //     e.target.remove();
-        // }
     })
+
+    target.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        console.log("draggable element is over the target")
+    });
+
+    target.addEventListener("drop", (e) => {
+        e.preventDefault();
+        const elementId = e.dataTransfer.getData("text/plain");
+        const draggedElement = document.getElementById(elementId);
+        console.log("draggable element has been dropped")
+        target.append(draggedElement);
+    });
 
     target.addEventListener("dragleave", (e) => {
         console.log("draggable element has left")
@@ -27,9 +35,27 @@ document.addEventListener("DOMContentLoaded", () => {
         insideDropTarget = false;
 
     })
-    draggableElement.addEventListener("dragend", (e) => {
-        console.log("element has stopped dragging", e);
+    // draggableElement.addEventListener("dragend", (e) => {
+    //     e.preventDefault();
+    //     console.log("element has stopped dragging", e);
 
+    // })
+
+    let fileInput = document.querySelector("#file-input");
+
+    fileInput.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        e.target.style.borderStyle = "dashed";
+        console.log("file is over the input");
+    })
+
+    fileInput.addEventListener("dragleave", (e) => {
+        e.target.style.borderStyle = "none";
+    })
+    fileInput.addEventListener("drop", (e) => {
+        e.preventDefault();
+        fileInput.files = e.dataTransfer.files;
+        e.target.style.borderStyle = "none";
 
     })
 
