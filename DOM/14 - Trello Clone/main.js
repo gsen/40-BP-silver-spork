335bb635702e689743a75cb1e0b77d9f96e9c1f9
Gaurav Sen <gaurav.s3n@gmail.com>
2026-01-25 18:34:25 +0530

    edit list functionality

--- DOM/14 - Trello Clone/main.js
+++ DOM/14 - Trello Clone/main.js
@@ -1,5 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    const DELETE_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>`;
+    const EDIT_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>`
+
     const STORAGE_KEYS = {
         todoApp: "todo-app"
     }
@@ -130,7 +134,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const deleteCard = document.createElement("span");
             deleteCard.classList.add("delete-card-btn");
-            deleteCard.textContent = "X"
+            deleteCard.innerHTML = DELETE_SVG;
             cardHeader.append(deleteCard);
 
             const cardBody = document.createElement("section");
@@ -177,10 +181,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const deleteListButton = document.createElement("span")
             deleteListButton.classList.add("delete-list-btn");
-            deleteListButton.textContent = "X";
+            deleteListButton.innerHTML = DELETE_SVG;
+
+            const editList = document.createElement("span");
+            editList.classList.add("list-edit-btn");
+            editList.innerHTML = EDIT_SVG;
 
 
-            header.append(title, deleteListButton);
+
+
+            header.append(title, editList, deleteListButton);
 
             const listItemsContainer = document.createElement("ul");
             listItemsContainer.classList.add("list-items");
@@ -226,6 +236,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 }))
 
 
+            } else if (event.target.closest(".list-edit-btn")) {
+                const newListName = prompt("Rename list", this.name);
+                if (newListName) {
+                    const title = event.target.closest(".list-edit-btn").previousElementSibling;
+                    title.textContent = newListName;
+
+                    this.name = newListName;
+                    this.syncToStore();
+                }
+
             }
         }
 
@@ -241,8 +261,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const cardDialog = document.querySelector("#new-card-dialog");
             cardDialog.dispatchEvent(customEvent);
-
-
 
         }
 
