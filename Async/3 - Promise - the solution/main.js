0eca9deff60113267b1b9aabc0f4bea8db6ed490
Gaurav Sen <gaurav.s3n@gmail.com>
2026-02-04 19:53:13 +0530

    promises with ui integration examples

--- Async/3 - Promise - the solution/main.js
+++ Async/3 - Promise - the solution/main.js
@@ -1,12 +1,14 @@
 const jobPromise = new Promise((resolve, reject) => {
 
     setTimeout(() => {
-        resolve("job lag gayi");
+        // resolve("job lag gayi");
         reject("paise gaye")
     }, 2000); // represents 8 months
 
 
 });
+
+console.log(jobPromise);
 console.log("mujhe 8 months do, job lagne ka promise");
 // success outcome
 // jobPromise.then((result) => {
@@ -39,6 +41,27 @@ jobPromise.then((result) => {
     console.log(result);
 }, error =>
     console.error(error))
+
+
+console.log("some other work is going on")
+console.log("next step")
+
+// promise states
+
+// pending - outcome is not yet known
+
+// fulfilled - successfull outcome - resolve called
+
+// reject - error/unsuccesfull case - reject called
+
+// settled - outcome is known now - i.e either fulfilled or rejected
+
+
+// promise chaining
+
+
+
+
 
 
 
