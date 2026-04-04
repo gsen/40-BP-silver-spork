320294076a39fa69af350cdfd605b3eb0e437696
Gaurav Sen <gaurav.s3n@gmail.com>
2026-04-04 15:03:32 +0530

    implemented basic login and registration

--- React & Node JS/18 - Authentication Project/auth-backend/controllers/auth-controller.mjs
+++ React & Node JS/18 - Authentication Project/auth-backend/controllers/auth-controller.mjs
@@ -1,12 +1,24 @@
-export function registerUser(req, res) {
+import { hash, compare } from "bcrypt"
+import { createNewUser, getUser } from "../models/users.mjs";
+const SALT_ROUNDS = 10;
+export async function registerUser(req, res) {
 
     const { username, password, name } = req.body;
     console.log({ username, password, name });
-
-    res.send({ username })
+    const hashedPassword = await hash(password, SALT_ROUNDS);
+    const result = await createNewUser({ username, password: hashedPassword, name })
+    res.send(result)
 
 }
 
-export function login(req, res) {
+export async function login(req, res) {
+    const { username, password } = req.body;
 
+    const existingUser = await getUser(username);
+    const result = await compare(password, existingUser.password)
+    if (result) {
+        res.send('match')
+    } else {
+        res.send('Invalid username or password!');
+    }
 }
---
+++ React & Node JS/18 - Authentication Project/auth-backend/models/users.mjs
@@ -0,0 +1,15 @@
+import { getDB } from "../db.mjs";
+
+function getCollection() {
+    return getDB().collection("users");
+}
+
+export async function createNewUser({ username, password, name }) {
+    const collection = getCollection();
+    return collection.insertOne({ username, password, name })
+}
+
+export function getUser(username) {
+    const collection = getCollection();
+    return collection.findOne({ username })
+}
