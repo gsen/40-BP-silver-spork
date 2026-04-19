1169c35c49488b72b6f59803db494883a0553480
Gaurav Sen <gaurav.s3n@gmail.com>
2026-04-19 12:21:57 +0530

    added course and lecture schemas

---
+++ learning-management-system/lms-backend/models/course.mjs
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const courseSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    intructor: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    lectures: [{
+        type: Schema.Types.ObjectId, ref: 'Lecture'
+    }],
+    thumbnail: String,
+    studentsEnrolled: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+
+}, { timestamps: true })
+
+export default model('Course', courseSchema);
