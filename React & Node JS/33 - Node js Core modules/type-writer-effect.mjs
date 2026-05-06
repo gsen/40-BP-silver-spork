8511349361f77036523b33673a3157158ddfd0cb
Gaurav Sen <gaurav.s3n@gmail.com>
2026-05-06 18:58:04 +0530

    typewrite effect example

---
+++ React & Node JS/33 - Node js Core modules/type-writer-effect.mjs
@@ -0,0 +1,44 @@
+import { createReadStream, read } from "node:fs";
+
+function readFileUsingStreaming() {
+    const filePath = new URL("./nodejs-large-stream-demo.txt", import.meta.url);
+
+    const readStream = createReadStream(filePath, {
+        encoding: "utf-8",
+        highWaterMark: 64 * 1024
+    });
+
+    let chunkCount = 0;
+
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    async function typeText(text) {
+        for (const char of text) {
+            process.stdout.write(char);
+            await delay(1);
+        }
+    }
+
+    readStream.on("data", async (chunk) => {
+        readStream.pause();
+        chunkCount++;
+        console.log(chunkCount);
+        console.log("Chunk size: ", chunk.length);
+        await typeText(chunk);
+        readStream.resume();
+    });
+
+    readStream.on("end", () => {
+        console.log("finished reading the file");
+    })
+
+    readStream.on("error", (err) => {
+        console.log("Error while reading file", err.message);
+    })
+
+    readStream.on("close", () => {
+        console.log("stream closed");
+    })
+}
+
+readFileUsingStreaming();
